74968ea2626fc8250efe837587048bf527d20141
Luwnto <luwnto@qq.com>
2024-04-25 15:06:47 +0800

    fix: 修复刷新页面导致naive-ui样式后加载的Bug

--- src/plugins/naive-ui.ts
+++ src/plugins/naive-ui.ts
@@ -3,6 +3,25 @@ import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin(nuxtApp => {
   if (process.server) {
+    // 解决刷新样式错乱的问题
+    nuxtApp.ssrContext?.head.hooks.hook('tags:resolve', (ctx) => {
+      //  insert Style after meta
+      const lastMetaIndex = ctx.tags.map((x) => x.tag).lastIndexOf('meta')
+      const styleTags = collect()
+        .split('</style>')
+        .filter(Boolean)
+        .map((x) => {
+          const id = x.match(/cssr-id="(.+?)"/)?.[1]
+          const style = (x.match(/>(.*)/s)?.[1] || '').trim()
+          return {
+            tag: 'style',
+            props: { 'cssr-id': id },
+            innerHTML: style
+          }
+        })
+      ctx.tags.splice(lastMetaIndex + 1, 0, ...styleTags)
+    })
+    
     const { collect } = setup(nuxtApp.vueApp)
     const originalRenderMeta = nuxtApp.ssrContext?.renderMeta
     nuxtApp.ssrContext = nuxtApp.ssrContext || {}
